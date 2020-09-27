4a729737aca1fe2c2a2420ddba69a458fa169317
OliWeirdo <oliweirdo@gmail.com>
2020-09-27 20:31:05 +0200

    Add project files.

---
+++ DrinkWater/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using DrinkWater.ViewModels;
+
+namespace DrinkWater.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ DrinkWater/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using DrinkWater.Models;
+using DrinkWater.ViewModels;
+
+namespace DrinkWater.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
