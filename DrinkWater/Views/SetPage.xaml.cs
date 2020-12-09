324a3d392cb70e282c4cc0ddc8c9c729628ee30a
OliWeirdo <oliweirdo@gmail.com>
2020-12-09 20:37:40 +0100

    Draft for reccurring method

---
+++ DrinkWater/Views/SetPage.xaml.cs
@@ -0,0 +1,150 @@
+﻿using DrinkWater.Notifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DrinkWater.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SetPage : ContentPage
+    {
+        readonly INotificationManager notificationManager;
+        private Timer _timer;
+        DateTime _triggerTime;
+        DateTime _triggerTimeEnd;
+        int _repeatTime = 1;
+        public SetPage()
+        {
+            InitializeComponent();
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), OnTimerTick);
+
+            notificationManager = DependencyService.Get<INotificationManager>();
+            notificationManager.NotificationReceived += (sender, eventArgs) =>
+            {
+                var evtData = (NotificationEventArgs)eventArgs;
+                ShowNotification(evtData.Title, evtData.Message);
+            };
+        }
+        //https://stackoverflow.com/questions/39571531/c-sharp-best-way-to-schedule-recurring-task-in-an-efficient-windows-service
+
+        private void RepeatNotification(object state)
+        {
+            try
+            {
+                OnTimerTick();
+            }
+            finally
+            {
+                _timer?.Change(_repeatTime, Timeout.Infinite);
+            }
+        }
+
+
+ 
+    
+    bool OnTimerTick()
+        {
+            if (_switch.IsToggled && DateTime.Now >= _triggerTime)
+                {
+                ShowNotification();
+            }
+            return true;
+        }
+
+        void ShowNotification()
+        {
+            //_switch.IsToggled = false;
+            string title = $"Drink Water";
+            string message = $"Time to drink water :)";
+            notificationManager.ScheduleNotification(title, message);
+
+            var duration = TimeSpan.FromSeconds(1);
+            Vibration.Vibrate(duration);
+        }
+
+        void OnTimePickerPropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == "Time")
+            {
+                SetTriggerTime();
+                SetTriggerTimeEnd();
+            }
+        }
+        void OnSwitchToggled(object sender, ToggledEventArgs args)
+        {
+            SetTriggerTime();
+            SetTriggerTimeEnd();
+        }
+        void SetTriggerTime()
+        {
+            if (_switch.IsToggled)
+            {
+                _triggerTime = DateTime.Today + _timePicker.Time;
+                if (_triggerTime < DateTime.Now)
+                {
+                    _triggerTime += TimeSpan.FromMinutes(_repeatTime);
+                }
+            }
+        }
+        void SetTriggerTimeEnd()
+        {
+            if (_switch.IsToggled)
+            {
+                _triggerTimeEnd = DateTime.Today + _timePickerEnd.Time;
+            }
+        }
+
+        void ShowNotification(string title, string message)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                var msg = new Label()
+                {
+                    Text = $"Notification Received:\nTitle: {title}\nMessage: {message}"
+                };
+
+            });
+        }
+
+        private void half_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            _repeatTime = 1;
+        }
+        private void one_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            _repeatTime = 60;
+        }
+        private void onehalf_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            _repeatTime = 90;
+        }
+        private void two_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            _repeatTime = 120;
+        }
+        private void twohalf_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            _repeatTime = 150;
+        }
+        private void three_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            _repeatTime = 180;
+        }
+        private void threehalf_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            _repeatTime = 210;
+        }
+        private void four_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            _repeatTime = 240;
+        }
+    }
+}
