3f8a7ea2d9b0fae1f9d3f91b4fd9a0e4b996921d
Oliweirdo <OliWeirdo@gmail.com>
2021-06-13 20:08:59 +0200

    cleaned needless elements

--- DrinkWater.Android/MainActivity.cs
+++ DrinkWater.Android/MainActivity.cs
@@ -7,8 +7,6 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Xamarin.Forms;
-using DrinkWater.Droid.Notifications;
-using DrinkWater.Notifications;
 using Android.Content;
 
 namespace DrinkWater.Droid
@@ -22,32 +20,17 @@ namespace DrinkWater.Droid
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
-            Forms.SetFlags(new string[] { "Expander_Experimental", "RadioButton_Experimental" });
             base.OnCreate(savedInstanceState);
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
+
+            Forms.SetFlags(new string[] { "Expander_Experimental", "RadioButton_Experimental" });
 
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
-            CreateNotificationFromIntent(Intent);
+
         }
 
-        protected override void OnNewIntent(Intent intent)
-        {
-            CreateNotificationFromIntent(intent);
-        }
-
-        void CreateNotificationFromIntent(Intent intent)
-        {
-            if (intent?.Extras != null)
-            {
-                string title = intent.GetStringExtra(AndroidNotificationManager.TitleKey);
-                string message = intent.GetStringExtra(AndroidNotificationManager.MessageKey);
-                DependencyService.Get<INotificationManager>().ReceiveNotification(title, message);
-            }
-        }
+    
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
